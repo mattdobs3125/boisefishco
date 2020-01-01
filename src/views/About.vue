5d04f6417b9064e5feb8dca8756bc8b6088df66d
matt <matthew.dobrowolski5@gmail.com>
2020-01-01 02:40:40 -0700

    adding some new stuff

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
+
+  
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div id="app">
+            <div>
+                <input type="text" v-model="name" placeholder="Your name?" />
+                <h5>Hello, {{name}}</h5>
+            </div>
+        </div>
 </template>
+
+<script>
+
+ new Vue({
+                template: '#app',
+                data: function() {
+                    return {
+                        name: ''
+                    }
+                }
+            }).$mount('#app');
+</script>
+
+<style>
+
+</style>
